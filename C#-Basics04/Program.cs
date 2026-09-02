2c14b7894bbff722b811f993499d69a8feca5e10
mahmoud <10812023100221@stud.cu.edu.eg>
2026-09-02 06:40:25 +0300

    Ques 3 & 4

--- C#-Basics04/Program.cs
+++ C#-Basics04/Program.cs
@@ -1,49 +1,62 @@
-﻿namespace C__Basics04
+﻿using System.Threading.Channels;
+
+namespace C__Basics04
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            #region Ques 1
+        static void Main(string[] args){
+            //PrintWelcomeMessage();  ---> Ques 3
 
-            ////double[] prices = {25.5,40.0,33.75 };
-            //Console.WriteLine(prices[1]);
-
-            #endregion
-
-            #region Ques 2
-
-            //int[,] shelfCopies = { { 3, 5 }, { 1, 4 } };
-            //Console.WriteLine(shelfCopies[1, 0]);
-
-            #endregion
-
-            #region Ques 3
-            #endregion
-
-            #region Ques 4
-            #endregion
-
-            #region Ques 5
-            #endregion
-
-            #region Ques 6
-            #endregion
-
-            #region Ques 7
-            #endregion
-
-            #region Ques 8
-            #endregion
-
-            #region Ques 9
-            #endregion
-
-            #region Ques 10
-            #endregion
-
-            #region Ques 11
-            #endregion
+            //PrintBookTitle("Clean Code"); ---> Ques 4
         }
-    }
+        #region Ques 1
+
+        ////double[] prices = {25.5,40.0,33.75 };
+        //Console.WriteLine(prices[1]);
+
+        #endregion
+
+        #region Ques 2
+
+        //int[,] shelfCopies = { { 3, 5 }, { 1, 4 } };
+        //Console.WriteLine(shelfCopies[1, 0]);
+
+        #endregion
+
+        #region Ques 3
+        //public static void PrintWelcomeMessage() {
+        //    Console.WriteLine("Welcome to the Library!");
+        //}
+
+        #endregion
+
+        #region Ques 4
+
+        //public static void PrintBookTitle(string title) {
+        //    Console.WriteLine("Book title :" + title);
+        //}
+
+        #endregion
+
+        #region Ques 5
+        #endregion
+
+        #region Ques 6
+        #endregion
+
+        #region Ques 7
+        #endregion
+
+        #region Ques 8
+        #endregion
+
+        #region Ques 9
+        #endregion
+
+        #region Ques 10
+        #endregion
+
+        #region Ques 11
+        #endregion
+    }    
 }
