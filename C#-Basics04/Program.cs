42c1eae6235de9d9e83b5acb0c5c84b6d254a02a
mahmoud <10812023100221@stud.cu.edu.eg>
2026-09-02 06:24:29 +0300

    Ques 1 & 2

--- C#-Basics04/Program.cs
+++ C#-Basics04/Program.cs
@@ -5,9 +5,17 @@
         static void Main(string[] args)
         {
             #region Ques 1
+
+            ////double[] prices = {25.5,40.0,33.75 };
+            //Console.WriteLine(prices[1]);
+
             #endregion
 
             #region Ques 2
+
+            //int[,] shelfCopies = { { 3, 5 }, { 1, 4 } };
+            //Console.WriteLine(shelfCopies[1, 0]);
+
             #endregion
 
             #region Ques 3
