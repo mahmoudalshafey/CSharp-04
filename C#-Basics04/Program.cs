4cc26884f28dab2dcece12b8a6c646b8495f273e
mahmoud <10812023100221@stud.cu.edu.eg>
2026-09-02 06:07:00 +0300

    intialization of regions

--- C#-Basics04/Program.cs
+++ C#-Basics04/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Ques 1
+            #endregion
+
+            #region Ques 2
+            #endregion
+
+            #region Ques 3
+            #endregion
+
+            #region Ques 4
+            #endregion
+
+            #region Ques 5
+            #endregion
+
+            #region Ques 6
+            #endregion
+
+            #region Ques 7
+            #endregion
+
+            #region Ques 8
+            #endregion
+
+            #region Ques 9
+            #endregion
+
+            #region Ques 10
+            #endregion
+
+            #region Ques 11
+            #endregion
         }
     }
 }
